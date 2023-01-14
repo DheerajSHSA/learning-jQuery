80c848915e7de7cdbdbe2952173d08950c01e0d6
Dheeraj kashyap <Dheerajkashyap4889@gmail.com>
2023-01-14 22:44:07 +0530

    parents()

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 $(function () {
     // $("#list").find("li").css("background-color", "red")
-    $("#list").children("li").css("background-color", "red") 
+    // $("#list").children("li").css("background-color", "red")
+    $("#list").parents("body").css("background-color", "red") 
 })
