d8c18cbb65cd30ec1b1773196a64e08a494a59a9
Dheeraj kashyap <Dheerajkashyap4889@gmail.com>
2023-01-14 22:23:09 +0530

    Real life animation example

--- js/script.js
+++ js/script.js
@@ -1,7 +1,5 @@
-$(function () {
-  $(".red-box").fadeTo(1000, 0, function() {
-    $(".green-box").delay(1000).fadeTo(1000, .5);
-  });
-
-  $(".blue-box").fadeTo(1000, .8).delay(1000).fadeOut().delay(1000).fadeIn();
-});
+$(function() {
+  $(".lightbox").delay(500).fadeIn(1000).animate({
+    "opacity" : "100%"
+  }, 1000);
+}); 
