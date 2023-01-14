6fb376ac53eb6b64165942b8102bf251815a1554
Dheeraj kashyap <Dheerajkashyap4889@gmail.com>
2023-01-14 21:59:02 +0530

    Slide Up and Down

--- js/script.js
+++ js/script.js
@@ -1,5 +1,10 @@
 $(function () {
-  // $(".blue-box").hide();
-  // $(".blue-box").show();
-  
+  $(".blue-box").slideUp(2000);  
+  $(".blue-box").slideDown(2000);
+  $("p").hide();
+  $("p").slideDown(2000);
 });
+
+// $(".blue-box").hide();
+// $(".blue-box").show();
+  
