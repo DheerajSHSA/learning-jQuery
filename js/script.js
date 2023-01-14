218357eb2d3810264ab2edc9f4a0773a858bb954
Dheeraj kashyap <Dheerajkashyap4889@gmail.com>
2023-01-14 21:08:16 +0530

    Mini Challenge

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 $(function () {
-  // jQuery goes here...
+  $(".red-box").fadeTo(2000, .2);
+  $(".green-box").fadeTo(2000, .5);
+  $(".blue-box").fadeTo(2000, .8);
 
-  // Uncomment this line to fade out the red box on page load
-  $(".green-box").fadeOut(2000);
+  $(".blue-box").fadeToggle();
+
 });
