6f5abbffe6ee541b29c59b8dbbc0656ace1451e6
Dheeraj kashyap <Dheerajkashyap4889@gmail.com>
2023-01-14 21:50:50 +0530

    Show and Hide method

--- js/script.js
+++ js/script.js
@@ -1,8 +1,5 @@
 $(function () {
-  $(".red-box").fadeTo(2000, .2);
-  $(".green-box").fadeTo(2000, .5);
-  $(".blue-box").fadeTo(2000, .8);
-
-  $(".blue-box").fadeToggle();
-
+  // $(".blue-box").hide();
+  // $(".blue-box").show();
+  
 });
