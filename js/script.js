780adb012f3503f5697cc301e246754047930ca2
Dheeraj kashyap <Dheerajkashyap4889@gmail.com>
2023-01-14 22:46:30 +0530

    next()

--- js/script.js
+++ js/script.js
@@ -4,5 +4,6 @@ $(function () {
     // $("#list").parents("body").css("background-color", "red")
     // $("#list").parent().css("background-color", "red")
     // $("#list").siblings().css("background-color", "red")
-    $("#list").prev().css("background-color", "red") 
+    // $("#list").prev().css("background-color", "red") 
+    $("#list").next().css("background-color", "red")
 })
