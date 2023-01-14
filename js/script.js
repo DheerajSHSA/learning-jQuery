2f8c84da5d5775c49fe1b6015a5149d0faab24a6
Dheeraj kashyap <Dheerajkashyap4889@gmail.com>
2023-01-14 22:17:51 +0530

    Callback Functions

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 $(function () {
-  $(".red-box").fadeTo(1000, .2);
-  $(".green-box").delay(1000).fadeTo(1000, .5);
-  $(".blue-box").fadeTo(1000, .8).delay(1000).fadeOut().delay(1000).fadeIn();  
+  $(".red-box").fadeTo(1000, 0, function() {
+    $(".green-box").delay(1000).fadeTo(1000, .5);
+  });
+
+  $(".blue-box").fadeTo(1000, .8).delay(1000).fadeOut().delay(1000).fadeIn();
 });
