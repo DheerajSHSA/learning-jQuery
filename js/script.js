2ec9407de69950bfc950a3713b8083f18238deeb
Dheeraj kashyap <Dheerajkashyap4889@gmail.com>
2023-01-15 21:14:15 +0530

    deatch()

--- js/script.js
+++ js/script.js
@@ -1,4 +1,9 @@
 $(function () {
     // To remove all the direct chilren of form element which are not an input of type text or the br 
-    $("form").children().not("input:text, textarea, br").remove();
+    // $("form").children().not("input:text, textarea, br").remove();
+
+    var detachedItem =  $("li").detach();
+    $("#content").append(detachedItem)
+
+    // If you want to use the removed Item later use detach because it saves all the event binds that the item has
 });
