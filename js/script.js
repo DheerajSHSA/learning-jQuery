4526af6cee4ad20b0a71e8f6f58e5e0b31b94b61
Dheeraj kashyap <Dheerajkashyap4889@gmail.com>
2023-01-15 22:08:02 +0530

    addClass()

--- js/script.js
+++ js/script.js
@@ -1,11 +1,3 @@
 $(function() {
-    $(".gallery").css("display", "none");
-
-    // var redBox = $(".red-box");
-    // console.log(redBox.css("width"))
-    // console.log(redBox.width());
-
-    var properties = $("p").css(["font-size"]);
-    console.log(properties)
-
+    $("a").addClass("fancy-link");
 });
