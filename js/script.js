81fb0b62b0af9e6ab3d811b690ab7d8d4be52c31
Dheeraj kashyap <Dheerajkashyap4889@gmail.com>
2023-01-15 19:55:04 +0530

    Mini challenge remove()

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
 $(function () {
-    $("li").remove();
+    // To remove all the direct chilren of form element which are not an input of type text or the br 
+    $("form").children().not("input:text, textarea, br").remove();
 });
