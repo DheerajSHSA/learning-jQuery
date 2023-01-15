28e2279159fc587ee5678cd6b2a440bca3ce9abf
Dheeraj kashyap <Dheerajkashyap4889@gmail.com>
2023-01-15 21:19:47 +0530

    empty()

--- js/script.js
+++ js/script.js
@@ -2,8 +2,11 @@ $(function () {
     // To remove all the direct chilren of form element which are not an input of type text or the br 
     // $("form").children().not("input:text, textarea, br").remove();
 
-    var detachedItem =  $("li").detach();
-    $("#content").append(detachedItem)
+    // var detachedItem =  $("li").detach();
+    // $("#content").append(detachedItem)
 
     // If you want to use the removed Item later use detach because it saves all the event binds that the item has
+
+    // $("p:first").empty();
+    // empty() only clears the text and the child element
 });
