80a5049bb3bb27329957f7478a899a05a0e506c4
Dheeraj kashyap <Dheerajkashyap4889@gmail.com>
2023-01-15 19:32:09 +0530

    append() and appendTo()

--- js/script.js
+++ js/script.js
@@ -1,12 +1,5 @@
 $(function () {
-    // $("#list").find("li").css("background-color", "red")
-    // $("#list").children("li").css("background-color", "red")
-    // $("#list").parents("body").css("background-color", "red")
-    // $("#list").parent().css("background-color", "red")
-    // $("#list").siblings().css("background-color", "red")
-    // $("#list").prev().css("background-color", "red") 
-    // $("#list").next().css("background-color", "red")
-    $("li").not(function (index) {
-        return index % 2 === 0;
-    }).css("background-color", "red");
+    // $("ul ul:first").append("<li>Hello, World!</li> ")
+
+    $("<li>Hello, World</li>").appendTo($("ul ul:last"))
 });
