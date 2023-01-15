837faf597446e7463b858a17628aa8ccb49d3877
Dheeraj kashyap <Dheerajkashyap4889@gmail.com>
2023-01-15 22:13:41 +0530

    data()

--- js/script.js
+++ js/script.js
@@ -1,3 +1,16 @@
 $(function() {
-    $("a").addClass("fancy-link");
+    var gallery = $(".gallery");
+    var images = [
+        "images/laptop-mobile_small.jpg",
+        "images/laptop-on-table_small.jpg",
+        "images/people-office-group-team_small.jpg"
+    ];
+
+    gallery.data("availableImages", images);
+    console.log(gallery.data("availableImages"));
+    gallery.data("name", "awesome gallery");
+    console.log(gallery.data());
+    gallery.removeData("name");
+    console.log(gallery.data());
+
 });
