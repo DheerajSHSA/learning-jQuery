92c28a98ebab6e1642224f36db18867ca8af2bfa
Dheeraj kashyap <Dheerajkashyap4889@gmail.com>
2023-01-16 19:23:26 +0530

    Form Validated

--- js/script.js
+++ js/script.js
@@ -1,17 +1,55 @@
 $(function () {
-    // $("#checkbox").change(function () {
-    //     var isChecked = $(this).is(":checked");
-    //     if (isChecked) {
-    //         $(this).add("label[for='cb']").css("box-shadow", "0 0 4px #181")
-    //     }
-    //     else {
-    //         $(this).add("label[for='cb']").css("box-shadow", "0 0 4px #811")
-    //     }
-    // })
+  $("#form").submit(function (event) {
+    var name = $("#name").val();
+    var password = $("#password").val();
+    var message = $("#message").val();
+    var checked = $("#checkbox").is(":checked");
 
-    $("#selection").change(function(){
-        var selectedOption = $(this).find(":selected").text();
-        alert(selectedOption)
+    validateNameField(name, event);
+    validatePasswordField(password, event);
+    validateCheckbox(checked, event);
+  });
+});
 
-    });
-}); 
+
+function validateNameField(name, event) {
+  if (!isValid(name)) {
+    $("#name-feedback").text("Please Enter a Valid Name").css("color", "red");
+    event.preventDefault();
+  }
+  else {
+    $("#name-feedback").text("");
+  }
+}
+
+function validatePasswordField(password, event) {
+  if (!isValidPassword(password)) {
+    $("#password-feedback").text("Please Enter a Valid Password").css("color", "red");
+    event.preventDefault();
+  }
+  else {
+    $("#password-feedback").text("");
+  }
+}
+
+function validateCheckbox(status, event) {
+  if (status === false) {
+    $("#checkbox-feedback").text("Please check the check box").css("color", "red");
+    event.preventDefault();
+  }
+  else {
+    $("#checkbox-feedback").text("");
+  }
+}
+
+function isValid(name) {
+  if (name.length < 2)
+    return false;
+  return true;
+}
+
+function isValidPassword(password) {
+  if (password.length < 8)
+    return false;
+  return true;
+}
