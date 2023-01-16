38d02ba276505e9c61a97f4693f48c697b6c7665
Dheeraj kashyap <Dheerajkashyap4889@gmail.com>
2023-01-16 11:18:50 +0530

    Event Listener

--- js/script.js
+++ js/script.js
@@ -1,13 +1,8 @@
 $(function () {
-    // text(), html()
-    // var firstPar = $("p:first");
-    // console.log(firstPar.text());
-    // console.log(firstPar.html());
-    // console.log($("p").html());
+    $("#btn-click").click(function (event) {
+        console.log(event);
+        alert('Button was Clicked');
+    });
 
-    // firstPar.text("<strong>Hello, </strong>World!")
-    // firstPar.html("<strong>Hello, </strong>World!")
-
-    var firstPar = $("p:first");
-    firstPar.text(firstPar.text() + "This is just appended");
-});
+    
+}); 
