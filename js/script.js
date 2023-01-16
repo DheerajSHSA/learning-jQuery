10af1d4609010563174e68c5e4cd05c24ea6c0f6
Dheeraj kashyap <Dheerajkashyap4889@gmail.com>
2023-01-16 13:29:21 +0530

    Mini challenge- change() event

--- js/script.js
+++ js/script.js
@@ -1,20 +1,17 @@
 $(function () {
-    var inputFields = $("input:text, input:password, textarea");
-    inputFields.focus(function(){
-        $(this).css("box-shadow", "0 0 4px #666");
-    });
+    // $("#checkbox").change(function () {
+    //     var isChecked = $(this).is(":checked");
+    //     if (isChecked) {
+    //         $(this).add("label[for='cb']").css("box-shadow", "0 0 4px #181")
+    //     }
+    //     else {
+    //         $(this).add("label[for='cb']").css("box-shadow", "0 0 4px #811")
+    //     }
+    // })
 
-    inputFields.blur(function(){
-        $(this).css("box-shadow", "none");
-    });
+    $("#selection").change(function(){
+        var selectedOption = $(this).find(":selected").text();
+        alert(selectedOption)
 
-    $("#name").blur(function() {
-        var text = $(this).val();
-        if(text.length < 3)
-        {
-            $(this).css("box-shadow", "0 0 4px #811")
-        }   else{
-            $(this).css("box-shadow", "0 0 4px #181")
-        }
-    })
+    });
 }); 
